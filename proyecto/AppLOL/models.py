3d17827bcc27bd8afa761ad15edfa2fb67bbaeee
AgsutinLara <agustinx123@gmail.com>
2022-11-23 17:56:59 -0300

    AGREGO README

--- proyecto/AppLOL/models.py
+++ proyecto/AppLOL/models.py
@@ -5,3 +5,8 @@ class nuevoitem(models.Model):
     items=models.CharField(max_length=40)
     precio=models.DateField()
     cualidades=models.IntegerField()
+
+class PERSONAJES(models.Model):
+    campeon=models.CharField(max_length=40)
+    buffonerfeo=models.DateField()
+    modificaciones=models.IntegerField()
