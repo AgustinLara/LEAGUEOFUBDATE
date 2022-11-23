c2ec836ae7c435ff017d569171bb73f21e8b0bd8
AgsutinLara <agustinx123@gmail.com>
2022-11-23 20:29:11 -0300

    JUNGLA

--- proyecto/AppLOL/urls.py
+++ proyecto/AppLOL/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppLOL import views
 
 urlpatterns = [
-    path("inicio/", views.inicio) ,
+    path("", views.inicio) ,
     path("items/", views.items) ,
     path("campenes/", views.campeones) ,
 ]
